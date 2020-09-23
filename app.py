4e4836715b90d7df649c098f86895fe927d22319
luizagit <caraimanluiza@gmail.com>
2020-09-23 22:55:01 +0100

    connected Flask to MongoDB Atlas and performed a simple query test

--- app.py
+++ app.py
@@ -1,8 +1,27 @@
 import os
 from flask import Flask, render_template
+from flask_pymongo import PyMongo
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+
 
 app = Flask(__name__)
 
+""" 
+MONGO_URI was set locally for my localhost app development and testing.
+vim ~/.bashrc
+export MONGO_URI="MONGO_URL"
+:wr save and exit then enter command
+source ~/.bashrc
+"""
+
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.config["MONGO_DBNAME"] = "ms3_trial"
+
+mongo = PyMongo(app)
+
+USER_DATA = mongo.db.users.find_one({'email': "test1@test1.com"})
+print(USER_DATA)
 
 @app.route('/')
 def index():
