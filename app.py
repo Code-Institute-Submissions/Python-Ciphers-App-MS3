be063a648ee33a1b3aad90bf8f2903b0797733a0
luizagit <caraimanluiza@gmail.com>
2020-09-23 23:12:22 +0100

    created register.html template

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for, session
 from flask_pymongo import PyMongo
 from pymongo import MongoClient
 from bson.objectid import ObjectId
